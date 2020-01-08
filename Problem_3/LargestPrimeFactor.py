6f4a3c82470f6410a1010896dc254c3e5e1465a7
GavinMcGuigan <gavinmcguigan1979@gmail.com>
2020-01-08 11:04:32 +0100

    Problem 3 Solved - Improved

--- Problem_3/LargestPrimeFactor.py
+++ Problem_3/LargestPrimeFactor.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 """
 The prime factors of 13195 are 5, 7, 13 and 29.
 
@@ -5,12 +7,19 @@ What is the largest prime factor of the number 600851475143 ?
 """
 
 
-def is_it_prime(num: int) -> bool:
-    for n in range(2, num):
-        if not num % n:
-            break
-    else:
-        return True
+def check_if_prime(num_to_check: int) -> bool:
+    top_val = sqrt(num_to_check)
+    if not top_val.is_integer():
+        top_val = int(top_val)
+        if not top_val % 2:
+            top_val -= 1
+
+        for n in range(top_val, 1, -2):
+            if not num_to_check % n:
+                break
+        else:
+            if num_to_check % 2:
+                return True
     return False
 
 
@@ -22,7 +31,7 @@ def calc_largest_prime_fact(seed: int = 13195) -> int:
     while running_total != seed:
         if not running_num % factor:
             running_num //= factor
-            running_total *= factor if is_it_prime(factor) else running_total
+            running_total *= factor if check_if_prime(factor) else running_total
             print(f'{factor:<10} - {running_total} ')
         else:
             factor = 3 if factor == 2 else factor + 2
@@ -32,3 +41,4 @@ def calc_largest_prime_fact(seed: int = 13195) -> int:
 if __name__ == '__main__':
     f = calc_largest_prime_fact(600851475143)
     print(f"\nAnswer: {f}")
+
