c5dcabc7a6dd9ab55012064db149e8a9cb137683
GavinMcGuigan <gavinmcguigan1979@gmail.com>
2020-03-02 11:04:39 +0100

    Problem 71 Solved Wasn't too difficult in the end, once you do a little research.

---
+++ Problem_71/OrderedFractions.py
@@ -0,0 +1,46 @@
+from globs import *
+from fractions import Fraction
+
+"""
+    Consider the fraction, n/d, where n and d are positive integers. If n<d and HCF(n,d)=1, it is called a 
+    reduced proper fraction.
+
+    If we list the set of reduced proper fractions for d ≤ 8 in ascending order of size, we get:
+    
+    1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
+    
+    It can be seen that 2/5 is the fraction immediately to the left of 3/7.
+    
+    By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order of size, find the numerator 
+    of the fraction immediately to the left of 3/7.
+"""
+
+
+class Fr(Fraction):
+    def __repr__(self):
+        return f'{self.numerator}/{self.denominator}'
+
+
+def main():
+    """ Farey mediant method """
+
+    start, end = Fr(0, 1), Fr(1, 1)
+    result, n = 0, 1000000
+
+    while not result:
+        m = Fr((start.numerator + end.numerator), (start.denominator + end.denominator))
+
+        if m.denominator > n:
+            result = start.numerator
+
+        if m >= Fr(3, 7):
+            end = m
+        else:
+            start = m
+
+    return result
+
+
+if __name__ == '__main__':
+    answer = main()
+    show_answer(answer)
